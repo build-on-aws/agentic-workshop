ded7ee70aa334e8a83c4707c1d2c26449d5eb1da
Banjo Obayomi <bobayomi@amazon.com>
2025-02-07 13:59:04 -0500

    added sagemaker

---
+++ sagemaker_ai/bedrock_example.py
@@ -0,0 +1,49 @@
+# Use the native inference API to send a text message to Meta Llama 3.
+
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+
+# Create a Bedrock Runtime client in the AWS Region of your choice.
+client = boto3.client("bedrock-runtime", region_name="us-west-2")
+
+# Set the model ID, e.g., Llama 3 70b Instruct.
+# Should be arn:aws:sagemaker:us-east-1:ACCOUNT_ID:endpoint/jumpstart-dft-deepseek-llm-r1-disti-20250207-153847
+model_id = "REPLACE_WITH_ARN"
+
+# Define the prompt for the model.
+prompt = "Describe the purpose of a 'hello world' program in one line."
+
+# Embed the prompt in Llama 3's instruction format.
+formatted_prompt = f"""
+<|begin_of_text|><|start_header_id|>user<|end_header_id|>
+{prompt}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+"""
+
+# Format the request payload using the model's native structure.
+native_request = {
+    "prompt": formatted_prompt,
+    "max_gen_len": 512,
+    "temperature": 0.5,
+}
+
+# Convert the native request to JSON.
+request = json.dumps(native_request)
+
+try:
+    # Invoke the model with the request.
+    response = client.invoke_model(modelId=model_id, body=request)
+
+except (ClientError, Exception) as e:
+    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+    exit(1)
+
+# Decode the response body.
+model_response = json.loads(response["body"].read())
+
+# Extract and print the response text.
+response_text = model_response["choices"][0]["text"]
+print(response_text)
